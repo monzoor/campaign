90972165d1e7888c89315aa838a5ce5b8a185906
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2019-10-02 15:27:21 +0600

    Updates

---
+++ precache-manifest.9f32c9d5c23ed729af58857e520e372a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe0ae039e4edf3e5582bdfcdc4f3959",
+    "url": "/campaign/index.html"
+  },
+  {
+    "revision": "5683f3bfc96f26d285bd",
+    "url": "/campaign/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "86d0b0431c15470ee5d8",
+    "url": "/campaign/static/css/main.0d4e355e.chunk.css"
+  },
+  {
+    "revision": "5683f3bfc96f26d285bd",
+    "url": "/campaign/static/js/2.252313ce.chunk.js"
+  },
+  {
+    "revision": "86d0b0431c15470ee5d8",
+    "url": "/campaign/static/js/main.4549d4a7.chunk.js"
+  },
+  {
+    "revision": "a753249e83759fe1bf6d",
+    "url": "/campaign/static/js/runtime-main.956655f4.js"
+  }
+]);
